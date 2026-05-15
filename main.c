31dfc4da462b3687e8bb83d714ca9b69e6d80775
shanmsyauqi@gmail.com <shanmsyauqi@gmail.com>
2026-05-15 13:41:33 +0700

    Updt Dinamik Array di Program 1 + UI

--- main.c
+++ main.c
@@ -29,17 +29,20 @@ const char* namaKualitas(kualitas_t k)
 udara_t inputUdara()
 {
     udara_t udara;
-    printf("Apakah Hari ini Hujan?? (1/0): \n");
+    printf("Apakah Hari ini Hujan?? (1/0)         : ");
     scanf("%d", &udara.isHujan);
-    printf("Apakah hari ini angin kencang?? (1/0): \n");
+    printf("Apakah Hari ini Angin Kencang?? (1/0) : ");
     scanf("%d", &udara.angin_kencang);
-    printf("Masukkan Jumlah PM2.5: \n");
+    printf("Masukkan Jumlah PM2.5                 : ");
     scanf("%f", &udara.pm2_5);
     return udara;
 }
 
 udara_t klasifikasiUdara(udara_t dataUdara[], int n)
 {
+    printf("\n=============================================\n");
+    printf("           Report Kualitas Udara\n");
+    printf("=============================================\n");
     
     for(int i = 0; i < n; i++) {
         if(dataUdara[i].isHujan == 1 || dataUdara[i].angin_kencang == 1) {
@@ -63,39 +66,65 @@ udara_t klasifikasiUdara(udara_t dataUdara[], int n)
 }
 
 void MenuAwalInformasi(){
-    printf("=============== SISTEM KUALIFIKASI KUALTIAS UDARA ===============\n");
+    printf("\n=============== SISTEM KUALIFIKASI KUALTIAS UDARA ===============\n");
     printf("Informasi Program:\nProgram ini menyiapkan 2 fitur berupa pengecekan kualitas udara di \nsuatu daerah dan hitungan mundur sebelum kiamat iklim terjadi.\n");
     printf("=================================================================\n");
-    printf("Pilih Menu:\n1. Pengecekan Kualitas Udara\n2. Perhitungan kiamat iklim\nPilihan: ");
+    printf("Pilih Menu:\n1. Pengecekan Kualitas Udara\n2. Perhitungan kiamat iklim\n3. Exit\nPilihan: ");
+}
+
+void MenuInputUdara(int n){
+    printf("=============================================\n");
+    printf("Masukkan data udara hari ke-%d\n", n+1);
+    printf("=============================================\n");
 }
 int main()
 {
-    int jumlahData, pilihan;
+    int pilihan, input;
+    int size = 0, capacity = 3; 
 
+    do{
     MenuAwalInformasi();
     scanf("%d", &pilihan);
 
     switch(pilihan){
-        case 1:     
-            printf("\nMasukkan Jumlah Data Harian Udara: ");
-            scanf("%d", &jumlahData);
-
+        case 1:
+            //Inisialisasi Array Dinamis
             udara_t *dataUdara;
-            dataUdara = (udara_t*)calloc(jumlahData,sizeof(udara_t));
-
-            for(int i = 0; i < jumlahData; i++){
-                printf("\nMasukkan data udara hari ke-%d\n", i+1);
-                dataUdara[i] = inputUdara();
+            dataUdara = (udara_t*)calloc(capacity,sizeof(udara_t));
+            if(dataUdara == NULL){
+                printf("Alokasi Memori Gagal");
+                free(dataUdara);
+                return 1;
             }
 
-            printf("\n=== Report Kualitas Udara ===\n");
-            klasifikasiUdara(dataUdara,jumlahData); 
+            while(1){
+            if (input == 0){
+                break;
+            }
+
+            if (capacity == size){
+                capacity *= 2;
+                dataUdara = realloc(dataUdara, capacity*sizeof(udara_t));
+            }
+
+            //Input Data
+            MenuInputUdara(size);
+            dataUdara[size] = inputUdara();
+            size++;
+            printf("Lanjut mengisi? (1/0): ");
+            scanf("%d", &input);
+            }
+
+            //Output Data
+            klasifikasiUdara(dataUdara,size); 
+            free(dataUdara);
             break;
 
         case 2:
-
         break;
             }
-
+    } while (pilihan != 3);
+    
+    printf("Terimakasih Telah Menggunakan Program Ini");
     return 0;
 }
